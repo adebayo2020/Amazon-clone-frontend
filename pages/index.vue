4746391798ca9b40da46e34f7ff13581c709cd0e
adebayo2020 <ajibadeadebayoengr@gmail.com>
2020-06-22 13:46:16 +0100

    Product listing page update: added API call to request products

--- pages/index.vue
+++ pages/index.vue
@@ -24,16 +24,20 @@
 
     <div class="container-fluid browsing-history">
       <div class="row">
-        <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
+        <div
+          v-for="(product, index) in products"
+          :key="product._id"
+          class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb"
+        >
           <div class="history-box">
             <!-- Product image -->
             <a href="" class="a-link-normal">
-              <img src="" class="img-fluid" />
+              <img :src="product.photo" class="img-fluid" />
             </a>
             <!-- Product title -->
             <div class="a-spacing-top-base asin-title">
               <span class="a-text-normal">
-                <div class="p13n-sc-truncated">product title</div>
+                <div class="p13n-sc-truncated">{{ product.title }}</div>
               </span>
             </div>
             <!-- Product rating -->
@@ -53,7 +57,7 @@
             <!-- Product price -->
             <div class="a-row">
               <span class="a-size-base a-color-price">
-                <span class="p13n-sc-price">$23</span>
+                <span class="p13n-sc-price">${{ product.price }}</span>
               </span>
             </div>
             <!-- Product buttons -->
@@ -69,7 +73,19 @@
 </template>
 
 <script>
-export default {};
+export default {
+  // asyncData is fetching data before nuxt page is finished loading on the browser
+  // It is good for SEO because the data will be loaded first
+  async asyncData({ $axios }) {
+    try {
+      let response = await $axios.$get("http://localhost:3000/api/products");
+      console.log(response);
+      return {
+        products: response.products
+      };
+    } catch {}
+  }
+};
 </script>
 
 <style></style>
