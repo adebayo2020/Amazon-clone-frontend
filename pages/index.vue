3a929eff48fb4d67c34ea4abe65b9251db9d1493
adebayo2020 <ajibadeadebayoengr@gmail.com>
2020-06-22 14:29:54 +0100

    created a product form page

--- pages/index.vue
+++ pages/index.vue
@@ -83,7 +83,7 @@ export default {
       return {
         products: response.products
       };
-    } catch {}
+    } catch (err) {}
   }
 };
 </script>
---
+++ pages/product.vue
@@ -0,0 +1,82 @@
+<template>
+  <main>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
+          <div class="a-section">
+            <div class="a-spacing-top-medium"></div>
+            <h2 style="text-align: center">Add a new Product</h2>
+            <form>
+              <!-- Category Dropdown -->
+              <div class="a-spacing-top-medium">
+                <label>Category</label>
+                <select class="a-select-option">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                </select>
+              </div>
+
+              <!-- Owner Dropdown -->
+              <div class="a-spacing-top-medium">
+                <label>Owner</label>
+                <select class="a-select-option">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                </select>
+              </div>
+
+              <!-- Title Dropdown -->
+              <div class="a-spacing-top-medium">
+                <label>Title</label>
+                <input type="text" class="a-input-text" style="width: 100%" />
+              </div>
+
+              <!-- Price Input -->
+              <div class="a-spacing-top-medium">
+                <label>Price</label>
+                <input type="number" class="a-input-text" style="width: 100%" />
+              </div>
+
+              <!-- Description Textarea -->
+              <div class="a-spacing-top-medium">
+                <label>Description</label>
+                <textarea
+                  placeholder="Provide details such as a product description"
+                  style="width: 100%"
+                />
+              </div>
+
+              <!-- Photo File -->
+              <div class="a-spacing-top-medium">
+                <label>Add Photo</label>
+                <div class="a-row a-spacing-top-medium">
+                  <label class="choosefile-button">
+                    <i class="fal fa-plus"></i>
+                    <input type="file" />
+                    <p style="margin-top: -70px">name of the</p>
+                  </label>
+                </div>
+              </div>
+
+              <!-- Button Dropdown -->
+              <hr />
+              <div class="a-spacing-top-large">
+                <span class="a-button-register">
+                  <span class="a-button-inner">
+                    <span class="a-button-text">Add product</span>
+                  </span>
+                </span>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="col-sm-3"></div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
