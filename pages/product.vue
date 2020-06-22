8aa47b4ef3d842fae0ff518a12a78a2682915cb9
adebayo2020 <ajibadeadebayoengr@gmail.com>
2020-06-22 16:35:50 +0100

    Product Form Page POST API with Axios

--- pages/product.vue
+++ pages/product.vue
@@ -11,7 +11,7 @@
               <!-- Category Dropdown -->
               <div class="a-spacing-top-medium">
                 <label>Category</label>
-                <select class="a-select-option">
+                <select class="a-select-option" v-model="categoryID">
                   <option
                     v-for="category in categories"
                     :value="category._id"
@@ -25,7 +25,7 @@
               <!-- Owner Dropdown -->
               <div class="a-spacing-top-medium">
                 <label>Owner</label>
-                <select class="a-select-option">
+                <select class="a-select-option" v-model="ownerID">
                   <option
                     v-for="owner in owners"
                     :value="owner._id"
@@ -39,13 +39,34 @@
               <!-- Title Dropdown -->
               <div class="a-spacing-top-medium">
                 <label>Title</label>
-                <input type="text" class="a-input-text" style="width: 100%" />
+                <input
+                  type="text"
+                  class="a-input-text"
+                  style="width: 100%"
+                  v-model="title"
+                />
               </div>
 
               <!-- Price Input -->
               <div class="a-spacing-top-medium">
                 <label>Price</label>
-                <input type="number" class="a-input-text" style="width: 100%" />
+                <input
+                  type="number"
+                  class="a-input-text"
+                  style="width: 100%"
+                  v-model="price"
+                />
+              </div>
+
+              <!-- Stock Quantity Input -->
+              <div class="a-spacing-top-medium">
+                <label>Stock Quantity</label>
+                <input
+                  type="number"
+                  class="a-input-text"
+                  style="width: 100%"
+                  v-model="stockQuantity"
+                />
               </div>
 
               <!-- Description Textarea -->
@@ -54,6 +75,7 @@
                 <textarea
                   placeholder="Provide details such as a product description"
                   style="width: 100%"
+                  v-model="description"
                 />
               </div>
 
@@ -63,8 +85,8 @@
                 <div class="a-row a-spacing-top-medium">
                   <label class="choosefile-button">
                     <i class="fal fa-plus"></i>
-                    <input type="file" />
-                    <p style="margin-top: -70px">name of the</p>
+                    <input type="file" @change="onFileSelected" />
+                    <p style="margin-top: -70px">{{ fileName }}</p>
                   </label>
                 </div>
               </div>
@@ -74,7 +96,9 @@
               <div class="a-spacing-top-large">
                 <span class="a-button-register">
                   <span class="a-button-inner">
-                    <span class="a-button-text">Add product</span>
+                    <span class="a-button-text" @click="onAddProduct"
+                      >Add product</span
+                    >
                   </span>
                 </span>
               </div>
@@ -106,6 +130,43 @@ export default {
       };
     } catch (err) {
       console.log(err);
+    }
+  },
+  data() {
+    return {
+      categoryID: null,
+      ownerID: null,
+      title: "",
+      price: 0,
+      description: "",
+      selectedFile: null,
+      stockQuantity: 1,
+      fileName: ""
+    };
+  },
+
+  methods: {
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0];
+      this.fileName = event.target.files[0].name;
+    },
+
+    async onAddProduct() {
+      let data = new FormData();
+      data.append("title", this.title);
+      data.append("price", this.price);
+      data.append("description", this.description);
+      data.append("ownerID", this.ownerID);
+      data.append("stockQuantity", this.stockQuantity);
+      data.append("categoryID", this.categoryID);
+      data.append("photo", this.selectedFile, this.selectedFile.name);
+
+      let result = await this.$axios.$post(
+        "http://localhost:3000/api/products",
+        data
+      );
+
+      this.$router.push("/");
     }
   }
 };
